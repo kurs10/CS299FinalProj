9b3ca7bfbd6df024a56fc1c4fbd41363242db473
unknown <geriannag96@gmail.com>
2017-11-20 00:17:55 -0800

    Blocks should be working, and added hella comments to everything so I don't forget lol

--- Tetris.py
+++ Tetris.py
@@ -3,19 +3,23 @@ from pygame.locals import *
 
 # Window setup
 FPS = 25
-WINDOW_WIDTH = 640
-WINDOW_HEIGHT = 480
-BOX_SIZE = 20
-GAME_WIDTH = 10
-GAME_HEIGHT = 20
+WINDOW_WIDTH = 640      # Window 640 pixels wide
+WINDOW_HEIGHT = 480     # Winow 480 pixels high
+BOX_SIZE = 20           # 20x20 pixels
+GAME_WIDTH = 10         # Game board is 10 boxes wide
+GAME_HEIGHT = 20        # Game board is 20 boxes tall
 
 # Margins between the game and window edge
+    # Margin from window leftright = (Total Window width - Game Width)/2
 X_MARGIN = int((WINDOW_WIDTH - GAME_WIDTH * BOX_SIZE) / 2)
-TOP_MARGIN = WINDOW_HEIGHT - (GAME_HEIGHT * BOX_SIZE)
+    # Margin from top = (Total window height - Game Height) - 10
+    # 10 is the distance from board to bottom
+    # NEED TO FIX BECUASE NO MARGIN ON BOTTOM
+TOP_MARGIN = WINDOW_HEIGHT - (GAME_HEIGHT * BOX_SIZE) - 10
 
-# Piece moves one space every 0.15s when the left/right arrow key is held
+# Piece moves one space to the left or right every 0.15s when the left/right arrow key is held
 SIDEWAYS_MOVE_FREQ = 0.15
-# Piece moves one space every 0.1s when the down arrow key is held
+# Piece moves one space down every 0.1s when the down arrow key is held
 DOWN_MOVE_FREQ = 0.1
 
 # Color setup (RGB)
@@ -30,13 +34,19 @@ BLUE = (0, 0, 155)
 L_BLUE = (20, 20, 175)
 YELLOW = (155, 155, 0)
 L_YELLOW = (175, 175, 20)
+
 # light blue (cyan)
 # orange
 # purple
 
+# Block Colors
 COLORS = (BLUE, GREEN, RED, YELLOW)
+# Block Colors HighLight
 L_COLORS = (L_BLUE, L_GREEN, L_RED, L_YELLOW)
-assert len(COLORS) == len(L_COLORS)
+
+# If false, raise an assertionError Exception
+# Make sure each color has an equal ligher color
+assert len(COLORS) == len(L_COLORS),"Not all colors have a lighter equal"
 
 # Assigning colors to the game
 BORDER_COLOR = BLUE
@@ -45,11 +55,12 @@ TEXT_COLOR = WHITE
 TEXT_SHADOW_COLOR = GRAY
 
 # Piece setup ("x" is a box and "." is an empty space )
-# Each template contains the normal and rotated positions
+# Each template contains the normal and all rotated positions
 # These templates are stored into a dictionary
-TEMPLATE_WIDTH = 5
-TEMPLATE_HEIGHT = 5
 BLANK = "."
+TEMPLATE_WIDTH = 5      #Templates will be 5x5 the area of shape rotation
+TEMPLATE_HEIGHT = 5
+
 
 S_PIECE = [[".....",
             ".....",
@@ -159,6 +170,7 @@ PIECES = {"S": S_PIECE, "Z": Z_PIECE, "J": J_PIECE, "L": L_PIECE,
 
 # Sets up the game, runs it, and checks for quit
 def runGame():
+    # Create new game
     game = getBlankGame()
     lastMoveDownTime = time.time()
     lastMoveSidewaysTime = time.time()
@@ -167,14 +179,16 @@ def runGame():
     movingLeft = False
     movingRight = False
     score = 0
-    level, fallFreq = calcLevel( score )
+    # Calcuates the level and fall frequency based on score
+    # Level increase and Fall Frequency increase the higher the score
+    level, FALL_FREQ = calcLevel( score )
 
     curPiece = getPiece()
     nextPiece = getPiece()
 
     # Game loop
     while True:
-        # Checks if there is no falling piece and starts a new piece at the top if there isn't
+        # Checks if there is no current piece and starts a new piece at the top if there isn't
         if curPiece == None:
             curPiece = nextPiece
             nextPiece = getPiece()
@@ -185,24 +199,145 @@ def runGame():
                 return
 
         checkForQuit()
-
+        # Event handling (Pause Game,Rotate Piece, Move Piece Down)
         for event in pygame.event.get():
+            # If key is released
             if event.type == KEYUP:
-                # Pause the game
+                # If p key released
                 if (event.key == K_p):
+                    # Pause the game
+                    # Display board with black screen
                     DISPLAY_SURF.fill(BG_COLOR)
                     pygame.mixer.music.stop()
-                    showTextScreen("Paused")
+                    # Will show "Paused" until a key is pressed
+                    showTextScreen("Paused") 
+                    # Once a key is pressed , restart music
                     pygame.mixer.music.play(-1, 0, 0)
+                    # Resest times to current time
                     lastFallTime = time.time()
                     lastMoveDownTime = time.time()
                     lastMoveSidewaysTime = time.time()
+                # If left arrow key released
                 elif event.key == K_LEFT:
-                    movingLeft = False
+                    movingLeft = False      #No longer rotating
+                # If left arrow key released
                 elif event.key == K_RIGHT:
-                    movingRight = False
+                    movingRight = False     #No longer rotating
+                # If down arrow key released
                 elif event.key == K_DOWN:
+                    movingDown = False      #No longer moving doww
+            
+            #=====Code dealing with moving piecies=====START
+            # Else if a key is pressed
+            elif event.type == KEYDOWN:
+                # MOVE LEFT
+                # If left arrow key or a is pressed and new positin is a valid position
+                if (event.key == K_LEFT or event.key == K_a) and isValidPosition(game, curPiece, adjX=-1):
+                    # Update x position by 1 to the left
+                    curPiece['x'] -= 1
+                    movingLeft = True
+                    movingRight = False
+                    lastMoveSidewaysTime = time.time()
+                # MOVE RIGHT
+                # If right arrow key or d is pressed and new positin is a valid position
+                elif (event.key == K_RIGHT or event.key == K_d) and isValidPosition(game, curPiece, adjX=1):
+                    # Update x position by 1 to the right
+                    curPiece['x'] += 1
+                    movingRight = True
+                    movingLeft = False
+                    lastMoveSidewaysTime = time.time()
+
+                # TO MOVE DOWN
+                # If down arrow key or s is pressed 
+                elif (event.key == K_DOWN or event.key == K_s):
+                    movingDown = True
+                    # If new position is a valid position
+                    if isValidPosition(game, curPiece, adjY=1):
+                        #Update y position by 1 down
+                        curPiece['y'] += 1
+                    lastMoveDownTime = time.time()
+
+                # TO MOVE DOWN ALL THE WAY
+                # If space bar is pressed         
+                elif event.key == K_SPACE:
                     movingDown = False
+                    movingLeft = False
+                    movingRight = False
+                    # For each row in board starting at 1 row from the top
+                    for i in range(1, GAME_HEIGHT):
+                        # If new row is not a valid position then stop at i
+                        if not isValidPosition(game, curPiece, adjY=i):
+                            break
+                    # Set lowest valid row possibile as 1 row above row i
+                    curPiece['y'] += i - 1
+
+                # ROTATE CLOCKWISE
+                # If up arrow key or w is pressed 
+                elif (event.key == K_UP or event.key == K_w):
+                    # Update rotation key value by +1
+                    curPiece['rotation'] = (curPiece['rotation'] + 1) % len(PIECES[curPiece['shape']])
+                    # If rotation value is larger than num of rotations, roll over and set rotation value to 0
+                    if not isValidPosition(game, curPiece):
+                        # Revert rotation to previous
+                        curPiece['rotation'] = (curPiece['rotation'] - 1) % len(PIECES[curPiece['shape']])
+
+                # ROTATE COUNTERCLOCKWISE
+                # If q key is pressed
+                elif (event.key == K_q):
+                    # Update rotation key value by -1    
+                    curPiece['rotation'] = (curPiece['rotation'] - 1) % len(PIECES[curPiece['shape']])
+                    # If new rotation of piece is not a valid position
+                    if not isValidPosition(game, curPiece):
+                        # Revert rotation to previous
+                        curPiece['rotation'] = (curPiece['rotation'] + 1) % len(PIECES[curPiece['shape']])
+                                               
+        # HOLDING DOWN RIGHT OR LEFT ARROW KEY                
+        # If moving left or right true
+        # AND time elapsed since piece last moved sideways is greater than the sideways move frequecy
+        if (movingLeft or movingRight) and time.time() - lastMoveSidewaysTime > SIDEWAYS_MOVE_FREQ:
+            # If moving left and new postition(1 space to the right) is a valid position
+            if movingLeft and isValidPosition(game, curPiece, adjX=-1):
+                # Update x position by -1
+                curPiece['x'] -= 1
+            # If moving right and new postition(1 space to the right) is a valid position
+            elif movingRight and isValidPosition(game, curPiece, adjX=1):
+                # Update x position by -1
+                curPiece['x'] += 1
+            #Update last move sidways time to current time
+            lastMoveSidewaysTime = time.time()
+
+        # If piece is moving down
+        if movingDown:
+            # If time elapsed since peice last moved down is greater than the down move frequency
+            if time.time() - lastMoveDownTime > DOWN_MOVE_FREQ:
+                # If 1 row down is a valid position
+                if isValidPosition(game, curPiece, adjY=1):
+                    # Move piece down by 1
+                    curPiece['y'] += 1
+                    # Update last move down time
+                    lastMoveDownTime = time.time()
+
+        # If time elapsed since peice last fell is greater than its fall frequency
+        if time.time() - lastFallTime > FALL_FREQ:
+            # If piece is already in place (aka 1 row down is not valid position)
+            if not isValidPosition(game, curPiece, adjY=1):
+                # Add piece to gameboard
+                addToBoard(game, curPiece)
+                # Check if there are any full rows and delete them and update score
+                score += deleteFullRows(game)
+                # Update level and fall frequency based on score
+                level, FALL_FREQ = calcLevel(score)
+                # No more current piece
+                curPiece = None
+            # Else if piece is still falling
+            else:
+                # Move piece one row down
+                curPiece['y'] += 1
+                #Update last fall time
+                lastFallTime = time.time()
+
+
+        #=====Code dealing with moving piecies=====END
 
         # Draws the current game status
         DISPLAY_SURF.fill(BG_COLOR)
@@ -211,16 +346,22 @@ def runGame():
         drawNextPiece(nextPiece)
         if curPiece != None:
             drawPiece(curPiece)
+        # Updates entire screen
         pygame.display.update()
         FPS_CLOCK.tick(FPS)
 
 
-# Determines the level the player is on and how many seconds should pass until a falling piece
+# Determines the level the player is on and how many seconds should pass until current piece
 # falls into a space
 def calcLevel( score ):
+    # Level increases every 10 points/lines cleared, starts at level 1
     level = int( score / 10 ) + 1
-    fallFreq = 0.27 - (level * 0.02)
-    return level, fallFreq
+    # Blocks fall faster by 0.02 secs as level increases (but max speed reached at level 13)
+    if (level > 13):
+        FALL_FREQ = 0.27 - (13 * 0.02)
+    else:
+        FALL_FREQ = 0.27 - (level * 0.02)
+    return level, FALL_FREQ
 
 
 # Returns a random new piece
@@ -235,14 +376,18 @@ def getPiece():
 
 
 # Returns true if the piece is within the board and there are no collisions
+# adjX, adjY used to adjust piece coordiates to actual board coordinates
 def isValidPosition(game, piece, adjX=0, adjY=0):
     for x in range(TEMPLATE_WIDTH):
         for y in range(TEMPLATE_HEIGHT):
             isAboveBoard = y + piece["y"] + adjY < 0
+            # If the piece is above the board or is a blacnk space
             if isAboveBoard or PIECES[piece["shape"]][piece["rotation"]][y][x] == BLANK:
                 continue
+            # If a part of the piece is not located on board
             if not isOnBoard(x + piece["x"] + adjX, y + piece["y"] + adjY):
                 return False
+            # If a part of the pice is located on coordinate of board that is not blank (aka already occupied)
             if game[x + piece["x"] + adjX][y + piece["y"] + adjY] != BLANK:
                 return False
     return True
@@ -265,7 +410,7 @@ def getBlankGame():
 def drawGame(game):
     # Draws the board's border
     pygame.draw.rect(DISPLAY_SURF, BORDER_COLOR,
-                     (X_MARGIN - 3, TOP_MARGIN - 7, (GAME_WIDTH * BOX_SIZE) + 8, (GAME_HEIGHT * BOX_SIZE) * 8), 5)
+                     (X_MARGIN - 3, TOP_MARGIN - 7, (GAME_WIDTH * BOX_SIZE) + 8, (GAME_HEIGHT * BOX_SIZE) + 8), 5)
 
     # Fills the board's background
     pygame.draw.rect(DISPLAY_SURF, BG_COLOR, (X_MARGIN, TOP_MARGIN, BOX_SIZE * GAME_WIDTH, BOX_SIZE * GAME_HEIGHT))
@@ -279,7 +424,9 @@ def drawGame(game):
 # Draws the specified piece
 def drawPiece( piece, pixelX=None, pixelY=None ):
     shape = PIECES[piece["shape"]][piece["rotation"]]
+    # If no pixel coordinates given
     if pixelX == None and pixelY == None:
+        # Convert board coordiates to pixel coordinates
         pixelX, pixelY = convertToPixel( piece["x"], piece["y"] )
     for x in range( TEMPLATE_WIDTH ):
         for y in range( TEMPLATE_HEIGHT ):
@@ -289,20 +436,27 @@ def drawPiece( piece, pixelX=None, pixelY=None ):
 
 # Draws the next piece
 def drawNextPiece( piece, pixelX=None, pixelY=None ):
+    # Displays "Next:" on screen
     nextSurf = BASIC_FONT.render('Next:', True, TEXT_COLOR)
     nextRect = nextSurf.get_rect()
     nextRect.topleft = (WINDOW_WIDTH - 120, 80)
     DISPLAY_SURF.blit(nextSurf, nextRect)
+    # Draws the next piece on side of board
     drawPiece(piece, pixelX=WINDOW_WIDTH - 120, pixelY=100)
 
 
-# Draws a single box
+# Draws a single box given x,y board coordinated or x,y pixel coordinates
 def drawBox( x, y, color, pixelX=None, pixelY=None ):
+    # If no color given, exit function
     if color == BLANK:
         return
+    # If no pixel coordinates given
     if pixelX == None and pixelY == None:
+        # Convert booard coordinates to pixel coordinates
         pixelX, pixelY = convertToPixel( x, y )
+    # Draw box with darker color first 
     pygame.draw.rect( DISPLAY_SURF, COLORS[color], (pixelX + 1, pixelY + 1, BOX_SIZE - 1, BOX_SIZE - 1 ) )
+    # Draw box with lighter color (for shading)
     pygame.draw.rect( DISPLAY_SURF, L_COLORS[color], (pixelX + 1, pixelY + 1, BOX_SIZE - 4, BOX_SIZE - 4 ) )
 
 
@@ -312,13 +466,61 @@ def drawStatus( score, level ):
     scoreSurf = BASIC_FONT.render( "Score: %s" % score, True, TEXT_COLOR )
     scoreRect = scoreSurf.get_rect()
     scoreRect.topleft = (WINDOW_WIDTH - 150, 20)
-    DISPLAY_SURF.blit( scoreSurf, scoreRect )
+    DISPLAY_SURF.blit( scoreSurf, scoreRect )   #Draws scoreSurf at given rectangle on display
 
     # Draws level
     levelSurf = BASIC_FONT.render('Level: %s' % level, True, TEXT_COLOR)
     levelRect = levelSurf.get_rect()
     levelRect.topleft = (WINDOW_WIDTH - 150, 50)
-    DISPLAY_SURF.blit(levelSurf, levelRect)
+    DISPLAY_SURF.blit(levelSurf, levelRect) #Draws levelSurf at given rectangle on display
+
+
+# Adds piece permanelty to board once piece has landed
+def addToBoard(game, piece):
+    # For evrey box in 5x5 template
+    for x in range(TEMPLATE_WIDTH):
+        for y in range(TEMPLATE_HEIGHT):
+            # If box is not blank/ is part of piece
+            if PIECES[piece['shape']][piece['rotation']][y][x] != BLANK:
+                # Color in box located in board with pieces location
+                game[x + piece['x']][y + piece['y']] = piece['color']
+
+
+# Checks is a row is fully filled, return True is whole row is filled
+def isRowFull(game, y):
+    # For each box in row y
+    for x in range(GAME_WIDTH):
+        # If box is empty
+        if game[x][y] == BLANK:
+            # Row not full
+            return False
+    return True
+
+
+# Deletes all full rows on board and shifts rows down 
+def deleteFullRows(game):
+    rowsDel = 0
+    #Start at bottom row
+    y = GAME_HEIGHT - 1
+    while y >= 0:
+        # If row y is full
+        if isRowFull(game, y):
+            # Delete row y and shift all rows above it down 
+            # For all rows above row y
+            for pullDownY in range(y, 0, -1):
+                for x in range(GAME_WIDTH):
+                    # Set row as row above it
+                    game[x][pullDownY] = game[x][pullDownY-1]
+            # Set very top row as blank.
+            for x in range(GAME_WIDTH):
+                game[x][0] = BLANK
+            rowsDel += 1
+        # Else if row y is not full
+        else:
+            # Check next row up
+            y -= 1
+    # Return rows deleted to update score
+    return rowsDel
 
 
 # Converts the given xy coordinates of the board to pixel coordinates on the screen
@@ -328,7 +530,7 @@ def convertToPixel( x, y ):
 
 # Displays the text screen (generic text screen function)
 def showTextScreen(text):
-    # Displays large text (with shadow) until a key is pressed
+    # Draws the shadow for the text (For looks purposes)
     titleSurf, titleRect = makeTextObjs(text, BIG_FONT, TEXT_SHADOW_COLOR)
     titleRect.center = (int(WINDOW_WIDTH / 2), int(WINDOW_HEIGHT / 2))
     DISPLAY_SURF.blit(titleSurf, titleRect)
@@ -343,6 +545,7 @@ def showTextScreen(text):
     pressKeyRect.center = (int(WINDOW_WIDTH / 2), int(WINDOW_HEIGHT / 2) + 100)
     DISPLAY_SURF.blit(pressKeySurf, pressKeyRect)
 
+    # Once a key is pressed, stop displaying text
     while checkForKeyPress() == None:
         pygame.display.update()
         FPS_CLOCK.tick
@@ -357,9 +560,12 @@ def makeTextObjs(text, font, color):
 # Checks if a key is pressed
 def checkForKeyPress():
     checkForQuit()
+    # Get all KEYDOWNs and KEYUPs events
     for event in pygame.event.get([KEYDOWN, KEYUP]):
+        # Ignore KEYDOWN
         if event.type == KEYDOWN:
             continue
+        # Return KEYUP event
         return event.key
     return None
 
@@ -369,8 +575,10 @@ def checkForQuit():
     for event in pygame.event.get(QUIT):
         terminate()
     for event in pygame.event.get(KEYUP):
+        # If ESC key was pressed
         if event.key == K_ESCAPE:
             terminate()
+        # Else if KEYUP was not for ESC key, put back in event queue
         pygame.event.post(event)
 
 
@@ -384,20 +592,26 @@ def terminate():
 def main():
     global FPS_CLOCK, DISPLAY_SURF, BASIC_FONT, BIG_FONT
     pygame.init()
+    # Create a clock object to track time
     FPS_CLOCK = pygame.time.Clock()
+    # Initializes a window with given size 
     DISPLAY_SURF = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+    # Creates a new Font Object from a file
     BASIC_FONT = pygame.font.Font("freesansbold.ttf", 18)
     BIG_FONT = pygame.font.Font("freesansbold.ttf", 100)
+    #Sets the window title/name as "Tetris!"
     pygame.display.set_caption("Tetris!")
 
+    # Displays title
     showTextScreen("Tetris!")
+    
     while True:
-        # Plays background game music
         pygame.mixer.music.load("tetris.mid")
+        # Loop music indefinitely (-1)
         pygame.mixer.music.play(-1, 0.0)
         runGame()
         pygame.mixer.music.stop()
-        showTextScreen("Game Over")
+        showTextScreen("Game Over!")
 
 
 if __name__ == "__main__":
